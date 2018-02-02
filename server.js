13eec725c267ecefb564f89a4997b5b5fbcffaa7
Akash Philip <>
2018-02-02 18:13:53 +0530

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 8080))
+app.set('ip', (process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
